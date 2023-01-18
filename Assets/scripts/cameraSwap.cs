7e2ff34528f2293405f24672907ce0e8066d0895
DanielGres <110058074+DanielGres@users.noreply.github.com>
2023-01-18 11:33:41 +0100

    camera update
    much better performance

--- Assets/scripts/cameraSwap.cs
+++ Assets/scripts/cameraSwap.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class cameraSwap : MonoBehaviour
 {
     public Camera[] cam;
+    public GameObject[] test;
     int counter;
 
     void Start()
     {
         cam[0].enabled = true;
+        test[1].SetActive(false);
+        test[2].SetActive(false);
+        test[3].SetActive(false);
         counter = 1;
         
     }
@@ -20,8 +24,10 @@ public class cameraSwap : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C))
         {
             cam[counter].enabled = false;
+            test[counter].SetActive(false);
             counter++;
             if (counter == cam.Length) { counter = 0; }
+            test[counter].SetActive(true);
             cam[counter].enabled = true;
         }
     }
