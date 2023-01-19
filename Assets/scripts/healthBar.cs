cf3585d90b84432f8db4915e73ca8af96b1cf6a0
DanielGres <110058074+DanielGres@users.noreply.github.com>
2023-01-19 01:01:11 +0100

    Added damage visualization
    -found weird bug with fixed joints

---
+++ Assets/scripts/healthBar.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBar : MonoBehaviour
+{
+    Renderer cubeRenderer;
+    public float healthPoints = 20;
+
+    public Color colorFine;
+    public Color colorFucking;
+    public Color colorFuucking;
+    public Color colorFucked;
+
+    float maxHLTH;
+    void Start()
+    {
+        cubeRenderer = transform.GetComponent<Renderer>();
+        maxHLTH = healthPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthPoints = Mathf.PingPong(Time.time*2,maxHLTH);
+        if (healthPoints > maxHLTH*2 / 3)
+        {
+            cubeRenderer.material.SetColor("_Color", Color.Lerp(colorFucking, colorFine, (healthPoints-2*(maxHLTH/3))/(maxHLTH/3)));
+        }
+        else if(healthPoints > maxHLTH / 3)
+        {
+            cubeRenderer.material.SetColor("_Color", Color.Lerp(colorFuucking, colorFucking, (healthPoints - (maxHLTH / 3)) / (maxHLTH/3)));
+        }
+        else
+        {
+            cubeRenderer.material.SetColor("_Color", Color.Lerp(colorFucked, colorFuucking, healthPoints / (maxHLTH / 3)));
+        }
+    }
+}
